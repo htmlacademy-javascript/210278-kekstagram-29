75b0ffbbd194f3beb8320b9f6f6ff32be5e83d1f
Maria <maria@MacBook-Pro-Aleksey.local>
2023-07-03 00:46:36 +0600

    Правки линта

--- js/main.js
+++ js/main.js
@@ -1,40 +1,3 @@
-function generateObjects() {
-  let objects = [];
-
-  for(let i = 0; i <= 24; i++) {
-    const currentId = i + 1;
-
-    objects[i] = {
-      id: currentId,
-      url: 'photos/' + currentId + '.jpg',
-      description: descriptions[i],
-      likes: getRandom(15, 200),
-      comments: getRandomComments(getRandom(0, 30))
-    };
-  }
-
-  return objects;
-}
-
-function getRandom(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
-function getRandomComments(length) {
-  const comments = [];
-
-  for(let i = 0; i < length; i++) {
-    comments[i] = {
-      id: i + 1,
-      avatar: 'img/avatar-' + getRandom(1, 6) + '.svg',
-      message: allMessages[getRandom(0, 5)],
-      name: allNames[getRandom(0, 3)],
-    };
-  }
-
-  return comments;
-}
-
 const allMessages = [
   'Всё отлично!',
   'В целом всё неплохо. Но не всё.',
@@ -79,5 +42,42 @@ const descriptions = [
   'Что-то происходит'
 ];
 
+function generateObjects() {
+  const objects = [];
+
+  for(let i = 0; i <= 24; i++) {
+    const currentId = i + 1;
+
+    objects[i] = {
+      id: currentId,
+      url: `photos/${currentId}.jpg`,
+      description: descriptions[i],
+      likes: getRandom(15, 200),
+      comments: getRandomComments(getRandom(0, 30))
+    };
+  }
+
+  return objects;
+}
+
+function getRandom(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function getRandomComments(length) {
+  const comments = [];
+
+  for(let i = 0; i < length; i++) {
+    comments[i] = {
+      id: i + 1,
+      avatar: `img/avatar-${getRandom(1, 6)}.svg`,
+      message: allMessages[getRandom(0, 5)],
+      name: allNames[getRandom(0, 3)],
+    };
+  }
+
+  return comments;
+}
+
 const result = generateObjects();
-console.log(result);
+// console.log(result);
