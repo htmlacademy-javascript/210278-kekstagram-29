90a55198c85e6fb70598042c9a49cc921390ea9d
Keks <keks@htmlacademy.ru>
2023-07-02 21:56:04 +0300

    :heavy_check_mark: Сборка #3

---
+++ 3/js/functions.js
@@ -0,0 +1,36 @@
+//Функция для проверки длины строки
+
+function lengthCheck (input, maxLength) {
+  const lengthInput = input.length;
+  const lengthResult = lengthInput <= maxLength;
+  return lengthResult;
+}
+
+lengthCheck('Доброго всем вечера', 20);
+
+//console.log(lengthCheck('Всем добрый вечер ', 10));
+//console.log(lengthCheck('Всем добрый вечер ', 18));
+//console.log(lengthCheck('Всем добрый вечер ', 20));
+
+//Функция проверки палиндрома
+
+function palindromeCheck (input) {
+  const inputWithoutSpace = input.replaceAll(' ', '');
+  const inputToLower = inputWithoutSpace.toLowerCase();
+  let invertInput = '';
+
+  for (let i = inputToLower.length; i >= 1; i--) {
+    const currentLetter = inputToLower[i - 1];
+    invertInput = invertInput + currentLetter;
+  }
+
+  const palindromeResult = inputToLower === invertInput;
+  return palindromeResult;
+}
+
+palindromeCheck('Топор');
+
+//console.log(palindromeCheck('топот'));
+//console.log(palindromeCheck('ДовОд'));
+//console.log(palindromeCheck('Кекс'));
+//console.log(palindromeCheck('Лёша на полке клопа нашёл '));
