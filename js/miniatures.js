a1a3e4e53ecc7d08c447440a3d613e7c94a6e214
Maria <maria@MacBook-Pro-Aleksey.local>
2023-07-10 01:09:26 +0600

    Правки линта

--- js/miniatures.js
+++ js/miniatures.js
@@ -3,27 +3,24 @@ import {generateObjects} from './utils.js';
 function viewMiniatures() {
   const pictures = document.querySelector('.pictures');
 
-  let docFragment = document.createDocumentFragment();
+  const docFragment = document.createDocumentFragment();
 
-  let template = document.querySelector('#picture').content;
+  const template = document.querySelector('#picture').content;
 
   const datas = generateObjects();
-
-  console.log(template);
-  console.log(datas);
 
   for (let i = 0; i < datas.length; i++) {
     const element = template.cloneNode(true);
 
-    let img = element.querySelector('.picture__img');
+    const img = element.querySelector('.picture__img');
 
-    img.setAttribute("src", datas[i].url);
-    img.setAttribute("alt", datas[i].description);
+    img.setAttribute('src', datas[i].url);
+    img.setAttribute('alt', datas[i].description);
 
-    let likes = element.querySelector('.picture__likes');
+    const likes = element.querySelector('.picture__likes');
     likes.textContent = datas[i].likes;
 
-    let comments = element.querySelector('.picture__comments');
+    const comments = element.querySelector('.picture__comments');
     comments.textContent = datas[i].comments.length;
 
     docFragment.appendChild(element);
