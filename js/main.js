068c487294bbea88045ffba869d281c60546b85c
Maria <maria@MacBook-Pro-Aleksey.local>
2023-07-16 01:10:33 +0600

    Открытие и закрытие модального окна и его содержимое

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
 import {viewMiniatures} from './miniatures.js';
+import {viewBigPicture} from './big-picture.js';
+import {generateObjects} from './utils.js';
 
-viewMiniatures();
+const objects = generateObjects();
+
+viewMiniatures(objects);
+viewBigPicture(objects);
