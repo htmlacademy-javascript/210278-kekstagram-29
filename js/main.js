b2053078ca8dff2f6f26ed6db0d787d73eee2e16
Maria <maria@MacBook-Pro-Aleksey.local>
2023-07-16 23:25:25 +0600

    Рефакторинг модального окна фото

--- js/main.js
+++ js/main.js
@@ -1,8 +1,10 @@
 import {viewMiniatures} from './miniatures.js';
 import {viewBigPicture} from './big-picture.js';
+import {viewModal} from './modal.js';
 import {generateObjects} from './utils.js';
 
 const objects = generateObjects();
 
 viewMiniatures(objects);
+viewModal();
 viewBigPicture(objects);
