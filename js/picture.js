fa4131bcb676e4072c4d84068dfcfb421ac8a32f
Maria <maria@MacBook-Pro-Aleksey.local>
2023-08-02 14:41:48 +0600

    исправление загрузки списка комментариев

--- js/picture.js
+++ js/picture.js
@@ -5,9 +5,19 @@ const modal = document.querySelector('.big-picture');
 const body = document.querySelector('body');
 const commentsElement = document.querySelector('.social__comments');
 
+let visibleCount = 0;
+let savedObjects = {};
+
+const onCommentsClick = () => {
+  visibleCount = viewBatchComments(savedObjects, visibleCount);
+  updateCommentStatistics(visibleCount, savedObjects.comments.length);
+};
+
 const closeModal = () => {
   body.classList.remove('modal-open');
   modal.classList.add('hidden');
+  visibleCount = 0;
+  document.querySelector('.comments-loader').removeEventListener('click', onCommentsClick);
 };
 
 const viewModal = () => {
@@ -29,6 +39,8 @@ const viewModal = () => {
 };
 
 const viewPicture = (currentObject) => {
+  savedObjects = currentObject;
+
   viewModal();
 
   const img = document.querySelector('.big-picture__img img');
@@ -43,12 +55,7 @@ const viewPicture = (currentObject) => {
   const socialCaption = document.querySelector('.social__caption');
   socialCaption.textContent = currentObject.description;
 
-  let visibleCount = 0;
-
-  document.querySelector('.comments-loader').addEventListener('click', () => {
-    visibleCount = viewBatchComments(currentObject, visibleCount);
-    updateCommentStatistics(visibleCount, currentObject.comments.length);
-  });
+  document.querySelector('.comments-loader').addEventListener('click', onCommentsClick);
 
   commentsElement.innerHTML = '';
 
